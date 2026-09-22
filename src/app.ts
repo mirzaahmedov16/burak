ab53a07bd1c2222b2c33f62b68995d66e3230ac7
mirzaahmedov16 <mirzaahmedovv1@gmail.com>
2026-09-22 16:22:19 +0900

    feat:express integrations

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";  
+import path from "path";
+
+/** 1-ENTRANCE**/
+
+const app = express();
+console.log("__dirname:", __dirname);
+app.use(express.static(path.join(__dirname, "public")));
+
+/** 2-SESSIONS **/
+/** 3-VIEWS **/
+/** 4-ROUTES **/
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import mongoose from "mongoose";
+import app from "./app";
 
 mongoose
   .connect(process.env.MONGODB_URI as string)
   .then((data) => {
     console.log(" MongoDB connection successful");
     const port = process.env.PORT || 3003;
+    app.listen(port, function ()  {
+      console.log("DONE");
+    });
   })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
