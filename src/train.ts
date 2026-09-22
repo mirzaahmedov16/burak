39c796f9df9d67b5e3a6dc90909c298f43becc31
mirzaahmedov16 <mirzaahmedovv1@gmail.com>
2026-09-22 17:41:53 +0900

    feat: M task complated

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,15 @@
-console.log("EXUCUTED");
+// M TASK
+
+function getSquareNumbers(arr: number[]){
+    let result: { number: number; square: number }[] = [];
+    for (let i = 0; i < arr.length; i++) {
+       let number = arr[i];
+        let square = number * number;
+       result.push({ number, square });
+
+       
+    }
+    return result;
+}
+
+console.log(getSquareNumbers([1, 2, 3, 4, 5])); 
