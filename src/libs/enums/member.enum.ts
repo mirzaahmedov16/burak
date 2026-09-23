1fbc4dea3442ae6939db9c646e075fda77acf20c
mirzaahmedov16 <mirzaahmedovv1@gmail.com>
2026-09-23 16:09:38 +0900

    feat: develop member schema model

---
+++ src/libs/enums/member.enum.ts
@@ -0,0 +1,12 @@
+export enum MemberType {
+   
+    USER = "USER",
+    RESTAURANT = "RESTAURANT",
+    
+}
+
+export enum MemberStatus {
+    ACTIVE = "ACTIVE",
+    BLOCKED = "BLOCKED",
+    DELETED = "DELETED",
+}
---
+++ src/schema/Member.models.ts
@@ -0,0 +1,58 @@
+import mongoose,{Schema} from "mongoose";
+import { MemberType } from "../libs/enums/member.enum";
+import { MemberStatus } from "../libs/enums/member.enum";
+const memberSchema = new Schema({
+    memberType:{ 
+        type: String,
+        enum: MemberType,
+        default: MemberType.USER,
+    },
+
+    memberStatus:{
+        type: String,
+        enum: MemberStatus,
+        default: MemberStatus.ACTIVE,
+       
+    },
+
+     memberNick:{
+        type: String,
+        index: {unique: true, sparse: true},
+        required: true,
+    },
+
+     memberPhone:{
+        type: String,
+        index: {unique: true, sparse: true},
+        required: true,
+    },
+
+    memberPassword:{
+        type: String,
+        select: false,
+        required: true,
+    },
+
+    memberAddress:{
+        type: String,
+    },
+
+     memberDesc:{
+        type: String,
+    },
+
+     memberImage:{
+        type: String,
+    },
+
+     memberPoints:{
+        type: Number,
+        default: 0,
+    },
+},
+
+{timestamps: true} // UpdatedAt and CreatedAt fields will be automatically added to the schema
+
+);
+
+export default mongoose.model("Member", memberSchema);
